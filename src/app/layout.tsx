65b9644f8e0d2121064b1d24aa90f30062b46970
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-09-18 00:40:00 +0900

    :sparkles: feat: theme-provider 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import './globals.css';
+import { ThemeProvider } from '@/providers/theme-provider';
 
 export default function RootLayout({
   children,
@@ -9,7 +10,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko' className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
