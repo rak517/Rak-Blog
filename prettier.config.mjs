c63cca2c187994b372feb27d6e7cb3e588e13c7e
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-09-17 16:33:40 +0900

    :wrench: chore: ESLint, Prettier, lint-staged 설정

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+const prettierConfig = {
+  printWidth: 120,
+  endOfLine: 'auto',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default prettierConfig;
