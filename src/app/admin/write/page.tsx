0bbbc8579c5e5905e6c57d7eec67de19b849a76d
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-09-26 14:39:26 +0900

    :sparkles: feat: 글 작성 페이지 라우팅 구조 설정

---
+++ src/app/admin/write/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <div>새 글 admin/write</div>
+      <div>임시 글 admin/write?id=xxx</div>
+      <div>기존 글 수정 admin/write?id=xxx</div>
+    </>
+  );
+}
