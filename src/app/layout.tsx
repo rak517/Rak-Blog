81bd8fee1a2703a70235a55b768bea9e4a887d43
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-10-02 01:01:12 +0900

    :sparkles: feat: Footer 컴포넌트 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistMono } from 'geist/font/mono';
 import './globals.css';
 import { ThemeProvider } from '@/providers/theme-provider';
 import Header from '@/components/layout/Header/Header';
+import { Footer } from '@/components/layout/Footer/Footer';
 
 export default function RootLayout({
   children,
@@ -15,6 +16,7 @@ export default function RootLayout({
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
           <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
