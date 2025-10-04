48b1daa6cdd30bc2c3ce26c7b5aba20688e28894
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-10-04 16:01:51 +0900

    :wrench: chore: 임시 placeholder 이미지 설정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/seed/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
