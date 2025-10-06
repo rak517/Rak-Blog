8ddcfca97d610b7f6a7d6a873ceac3cc0e1072cc
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-10-06 10:47:16 +0900

    :sparkles: feat: /about 페이지 필요한 상수 정의

---
+++ src/constants/about.ts
@@ -0,0 +1,32 @@
+import { Github, Linkedin, Mail } from 'lucide-react';
+
+export const interests = [
+  'Frontend Development (React, Next.js, TypeScript)',
+  'UI/UX Design & Development',
+  'Web Performance Optimization',
+  'Modern JavaScript & CSS',
+  'Developer Experience (DX)',
+];
+
+export const contacts = [
+  {
+    label: 'Email',
+    value: 'tjdfkr01@gmail.com',
+    href: 'mailto:tjdfkr01@gmail.com',
+    icon: Mail,
+  },
+  {
+    label: 'GitHub',
+    value: 'github.com/rak517',
+    href: 'https://github.com/rak517',
+    icon: Github,
+    target: '_blank',
+  },
+  {
+    label: 'LinkedIn',
+    value: 'linkedin.com/in/rak517',
+    href: 'https://www.linkedin.com/in/rak517',
+    icon: Linkedin,
+    target: '_blank',
+  },
+];
